c17e08426b7074e3fe0603622543a59700758a95
Stefan Aleksic <stefan@stefanaleksic.com>
2015-11-08 19:05:44 -0500

    Add basic game mechanics

---
+++ src/game_objects.py
@@ -0,0 +1,66 @@
+class MovableCharacter:
+    def move(self,speed = 2):
+        direction = self.current_direction
+        if direction == Direction.UP:
+            self.y -= speed
+        elif direction == Direction.RIGHT:
+            self.x += speed
+        elif direction == Direction.DOWN:
+            self.y += speed
+        elif direction == Direction.LEFT:
+            self.x -= speed
+
+
+    def change_direction(self, direction, override_opt = False):
+        # Optimization
+        if not override_opt and self.current_direction == direction:
+            return
+        self.current_direction = direction
+        name = self.directional_sprites[direction]
+        self.sprite = self.load_function(name)
+
+    def __init__(self, name, load_function, directional_sprites,x=200,y=400, gold=0):
+        self.moving = False
+        self.load_function = load_function
+        self.directional_sprites = directional_sprites
+        self.x = x
+        self.y = y
+        self.name = name
+        self.gold = gold
+        self.current_direction = Direction.UP
+        
+class MainPlayer(MovableCharacter):
+    pass
+
+class EnemyPlayer(MovableCharacter):
+    pass
+
+class Direction:
+    UP = 0
+    RIGHT = 1
+    DOWN = 2
+    LEFT = 3
+
+class Element:
+    FIRE   = 1
+    NATURE = 2
+    WATER  = 3
+
+class GenericBot:
+    def __init__(self, name, image, speed=0, health=100,mana=100,element=Element.FIRE,spell_xp=dict(),list_of_spells=[]):
+        self.name = name
+        self.sprite = sprite 
+        self.speed = speed
+        self.health = health
+        self.mana = mana
+        self.element = element
+        self.spell_xp = spell_xp
+        self.list_of_spells = list_of_spells
+
+class Spells:
+    def __init__(self, name, mana_cost=25, attack_power=5,multiplier=dict(),accuracy=0.5):
+        self.name = name
+        self.mana_cost = mana_cost
+        self.attack_power = attack_power
+        self.multiplier = multiplier
+        self.accuracy = accuracy
---
+++ src/main.py
@@ -0,0 +1,72 @@
+import pygame, sys
+from game_objects import *
+from pygame.locals import *
+
+class Game:
+    def __init__(self,clock,surface,movable_characters,main_player,move_event):
+        self.clock = clock
+        self.surface = surface
+        self.movable_characters = movable_characters
+        self.main_player = main_player
+        self.move_event = move_event
+
+def initialize_game():
+    pygame.init()
+    clock = pygame.time.Clock()
+    move_event = pygame.USEREVENT+1
+    pygame.time.set_timer(move_event, 1000)
+    surface = pygame.display.set_mode((1200, 900), 0, 32)
+    movable_characters = []
+    main_player = MovableCharacter("P1",pygame.image.load, ["res/main_player/up.jpg", "res/main_player/right.jpg", "res/main_player/down.jpg", "res/main_player/left.jpg"])
+    main_player.change_direction(main_player.current_direction, override_opt = True)
+    movable_characters.append(main_player)
+    return Game(clock,surface,movable_characters,main_player, move_event)
+
+def keypress_event(event,main_player):
+    if event.type == KEYDOWN:
+        # Use change_direction instead of just changing the
+        # variable since it also changes the sprite image
+        main_player.moving = True
+        if event.key == K_UP:
+            main_player.change_direction(Direction.UP)
+        elif event.key == K_RIGHT:
+            main_player.change_direction(Direction.RIGHT)
+        elif event.key == K_DOWN:
+            main_player.change_direction(Direction.DOWN)
+        elif event.key == K_LEFT:
+            main_player.change_direction(Direction.LEFT)
+# run the game loop
+def game_loop():
+    FPS = 30 
+    game_instance = initialize_game()
+
+    surface = game_instance.surface
+    clock = game_instance.clock
+    movable_characters = game_instance.movable_characters
+    main_player = game_instance.main_player
+    move_event = game_instance.move_event
+
+    while True:
+        surface.fill((0,0,0))
+
+        for character in movable_characters:
+            if character.moving:
+                character.move()
+                
+            surface.blit(character.sprite, (character.x, character.y))
+
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                pygame.quit()
+                sys.exit()
+            elif event.type == move_event:
+                main_player.moving = False
+
+            keypress_event(event, main_player)
+
+        pygame.display.update()
+        clock.tick(FPS)
+        
+
+if __name__ == '__main__':
+    game_loop()
