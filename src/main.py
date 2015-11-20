2b5a80169f58dbcfe260945b7c6c0bf4bc9958fd
The Stick <avaera8820@gmail.com>
2015-11-19 19:44:04 -0500

    Think I've Sugarified it, still need to test it.

--- src/main.py
+++ src/main.py
@@ -1,77 +1,66 @@
-import pygame, sys
+import pygame
+from gi.repository import Gtk
 from game_objects import *
-from pygame.locals import *
 
-class Game:
-    def __init__(self,clock,surface,movable_characters,main_player):
-        self.clock = clock
-        self.surface = surface
-        self.movable_characters = movable_characters
-        self.main_player = main_player
+class GolemsGame:
+    def __init__(self):
+        self.clock = pygame.time.Clock()
+        self.movable_characters = []
+        self.main_player = MovableCharacter("P1",pygame.image.load, ["res/main_player/up.png", "res/main_player/right.png", "res/main_player/down.png", "res/main_player/left.png"])
+        self.main_player.change_direction(main_player.current_direction, override_opt = True)
+        movable_characters.append(main_player)
 
-def initialize_game():
-    pygame.init()
-    clock = pygame.time.Clock()
-    surface = pygame.display.set_mode((0, 0), pygame.RESIZABLE)
-    movable_characters = []
-    main_player = MovableCharacter("P1",pygame.image.load, ["res/main_player/up.png", "res/main_player/right.png", "res/main_player/down.png", "res/main_player/left.png"])
-    main_player.change_direction(main_player.current_direction, override_opt = True)
-    movable_characters.append(main_player)
-    return Game(clock,surface,movable_characters,main_player)
-
-def keypress_event(event,main_player):
-    if event.type == KEYDOWN:
-        if main_player.moving: # Player's currently moving, ignore keypresses
-			return;
+    def keypress_event(event,main_player):
+        if self.main_player.moving: # Player's currently moving, ignore keypresses
+            return;
         # Use change_direction instead of just changing the
         # variable since it also changes the sprite image
-        main_player.moving = True
+        self.main_player.moving = True
         if event.key == K_UP or event.key == K_KP8:
-            main_player.change_direction(Direction.UP)
+            self.main_player.change_direction(Direction.UP)
         elif event.key == K_RIGHT or event.key == K_KP6:
-            main_player.change_direction(Direction.RIGHT)
+            self.main_player.change_direction(Direction.RIGHT)
         elif event.key == K_DOWN or event.key == K_KP2:
-            main_player.change_direction(Direction.DOWN)
+            self.main_player.change_direction(Direction.DOWN)
         elif event.key == K_LEFT or event.key == K_KP4:
-            main_player.change_direction(Direction.LEFT)
+            self.main_player.change_direction(Direction.LEFT)
         else:
-            main_player.moving = False # If the player pushed an invalid key, don't move character.
-# run the game loop
-def game_loop():
-    FPS = 30 
-    game_instance = initialize_game()
+            self.main_player.moving = False # If the player pushed an invalid key, don't move character.
+    # run the game loop
+    def game_loop():
+        FPS = 30
+        surface = pygame.display.get_surface()
 
-    surface = game_instance.surface
-    clock = game_instance.clock
-    movable_characters = game_instance.movable_characters
-    main_player = game_instance.main_player
+        while True:
+            surface.fill((10,10,10))
+            
+            # Just some placeholder background / debugging grid show
+            for x in range(0, 1250, 50):
+                for y in range(0, 950, 50):
+                    pygame.draw.circle(surface, (128, 128, 128), (x, y), 4, 0)
 
-    while True:
-        surface.fill((10,10,10))
+            for character in self.movable_characters:
+                if character.moving:
+                    character.move()
+                    
+                surface.blit(character.sprite, (character.x, character.y))
+
+            for event in pygame.event.get():
+                if event.type == QUIT:
+                    return
+                elif event.type == pygame.VIDEORESIZE:
+                    pygame.display.set_mode(event.size, pygame.RESIZABLE)
+                elif event.type == pygame.KEYDOWN:
+                    keypress_event(event, main_player)
+
+            pygame.display.update()
+            clock.tick(FPS)
+     
+    # Called when loading a saved state from the Journal
+    def read_file(self, file_path):
+        #stub
         
-        # Just some placeholder background / debugging grid show
-        for x in range(0, 1250, 50):
-            for y in range(0, 950, 50):
-                pygame.draw.circle(surface, (128, 128, 128), (x, y), 4, 0)
+    # Called when saving a saved state to the Journal
+    def write_file(self, file_path):
+        #stub
 
-        for character in movable_characters:
-            if character.moving:
-                character.move()
-                
-            surface.blit(character.sprite, (character.x, character.y))
-
-        for event in pygame.event.get():
-            if event.type == QUIT:
-                pygame.quit()
-                sys.exit()
-            elif event.type == pygame.VIDEORESIZE:
-                pygame.display.set_mode(event.size, pygame.RESIZABLE)
-
-            keypress_event(event, main_player)
-
-        pygame.display.update()
-        clock.tick(FPS)
-        
-
-if __name__ == '__main__':
-    game_loop()
