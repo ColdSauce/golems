cbb5acce8f847b11c60c1fac98f267efe75e0686
Stefan Aleksic <stefan@stefanaleksic.com>
2015-11-28 01:33:48 -0500

    Add scene switching ability

--- src/main.py
+++ src/main.py
@@ -4,17 +4,19 @@ from game_objects import *
 from pygame.locals import *
 import sys
 
+SCREEN_WIDTH = 100
+SCREEN_HEIGHT = 100
 isLinux = sys.platform.startswith("linux")
 if(isLinux):
     from gi.repository import Gtk
 
 class SceneManager():
     def __init__(self):
-        self.go_to(TitleScene())
+        self.go_to(MenuScreen())
 
     def go_to(self, scene):
         self.scene = scene
-        self.scene_manager = self
+        self.scene.manager = self
 
 class Scene():
     def __init__(self):
@@ -30,9 +32,11 @@ class Scene():
         raise NotImplementedError
 
 class InteractiveScene(Scene):
-    def __init__(self, width, height):
-        self.width = width
-        self.height = height
+    def __init__(self):
+        global SCREEN_HEIGHT
+        global SCREEN_WIDTH
+        self.height = SCREEN_HEIGHT
+        self.width = SCREEN_WIDTH
         self.movable_characters = []
         self.map = Map(30, 15)
         self.main_player = MainPlayer(name = "P1",
@@ -81,8 +85,6 @@ class InteractiveScene(Scene):
             if(not self.map.isSolid(char.gridX - 1, char.gridY)):
                 char.moving = True
 
-
-
     def render(self, surface):
         surface.fill((0,0,0))
         self.map.render(surface, self.main_player.gridX * 50 + self.main_player.xOffset, self.main_player.gridY * 50 + self.main_player.yOffset)
@@ -128,13 +130,22 @@ class InteractiveScene(Scene):
 
 class MenuScreen(Scene):
     def __init__(self):
-        pass
-    def render(self, screen):
-        pass
+        self.font_color = (200,200,200)
+        self.font = pygame.font.SysFont("comicsansms", 500)
+        self.label = self.font.render("Play!", 1, self.font_color)
+    def render(self, surface):
+        surface.fill((0,0,0))
+        surface.blit(self.label, (100,100))
     def update(self):
         pass
     def handle_events(self,events):
-        pass
+        for event in events:
+            print "event is " + str(event)
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                print "this works"
+                self.manager.go_to(InteractiveScene())
+
+
 
 class GolemsGame:
     
@@ -143,25 +154,30 @@ class GolemsGame:
                 
         # run the game loop
     def game_loop(self):
+        global SCREEN_WIDTH
+        global SCREEN_HEIGHT
         FPS = 30
         surface = pygame.display.get_surface()
         width, height = surface.get_size()
-        self.scene = InteractiveScene(width, height)
+        SCREEN_WIDTH = width
+        SCREEN_HEIGHT = height
 
+        manager = SceneManager()
         while True:
+
             if(isLinux):
                 while Gtk.events_pending():
                     Gtk.main_iteration()
-
-            for event in pygame.event.get():
+            events = pygame.event.get()
+            for event in events:
                 if event.type == pygame.QUIT:
                     return
                 elif event.type == pygame.VIDEORESIZE:
                     pygame.display.set_mode(event.size, pygame.RESIZABLE)
 
-            self.scene.update()
-            self.scene.render(surface)
-            self.scene.handle_events(pygame.event.get())
+            manager.scene.handle_events(events)
+            manager.scene.update()
+            manager.scene.render(surface)
 
             pygame.display.update()
             self.clock.tick(FPS)
