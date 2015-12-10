2088666e3968df1f37e90ae7ac16df77b9cedc4c
The Stick <avaera8820@gmail.com>
2015-12-10 17:44:49 -0500

    Now there's an arrow that goes up and down the list

--- src/codingscene.py
+++ src/codingscene.py
@@ -5,17 +5,30 @@ class CodingScene(scene.Scene):
         self.char = mainChar
         self.font = pygame.font.SysFont("couriernew", 24)
         self.keysLastFrame = None
+        self.currentArrowIndex = 0
+        self.totalArrowCount = 0
+        self.menuIndex = 0
     def render(self, surface):
         surface.fill((0,0,0))
         top = 16
+        arrIndex = self.currentArrowIndex
         for block in self.char.list_of_bots[0].queue_of_code_blocks:
-            top += block.render(surface, 660, top) 
+            top += block.render(surface, 16, top, arrIndex) 
+            arrIndex -= block.getArrowCount()
     def doKeys(self):
         keys = pygame.key.get_pressed()
-        if kbInput.isBackPressed(keys) and not kbInput.isBackPressed(self.keysLastFrame):
-            self.manager.go_to(scene.Scenes.INTERACTIVE)
+        else:
+            if kbInput.isBackPressed(keys) and not kbInput.isBackPressed(self.keysLastFrame):
+                self.manager.go_to(scene.Scenes.INTERACTIVE)
+            if kbInput.isDownPressed(keys) and not kbInput.isDownPressed(self.keysLastFrame):
+                self.currentArrowIndex = min(self.currentArrowIndex + 1, self.totalArrowCount - 1)
+            if kbInput.isUpPressed(keys) and not kbInput.isUpPressed(self.keysLastFrame):
+                self.currentArrowIndex = max(self.currentArrowIndex - 1, 0)
         self.keysLastFrame = keys
     def update(self):
+        self.totalArrowCount = 0
+        for block in self.char.list_of_bots[0].queue_of_code_blocks:
+            self.totalArrowCount += block.getArrowCount()
         self.doKeys()
     def handle_events(self, events):
         for event in events:
